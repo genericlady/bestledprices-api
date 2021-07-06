a804b92cf764897abd50933d9342c9b1e7bec031
yianna <yiannakokalas@gmail.com>
2021-07-05 20:37:54 -0700

    Add port for production

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { Request, Response } from "express";
 import { Browser, Page } from "puppeteer";
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const host = "https://www.adafruit.com";
 const allowedOrigins = ["http://localhost:3001"];
 
